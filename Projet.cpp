81a63e35ac4e43b731256bfd5dbbdbda396934ca
Cyrille <cyrille@cyrille-GE60-2PE>
2018-03-24 18:06:07 +0100

    Normalement j'ai à peu pres commenté

--- Projet.cpp
+++ Projet.cpp
@@ -3,23 +3,124 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+
+
+#include <sstream>
 
 using namespace llvm;
+int i = 0;
+
+void printStr(char *toPrint) {
+  if (toPrint) {
+    fprintf(stderr, "%s", toPrint);
+  }
+  else {
+    fprintf(stderr, "::printStr(...):NULL arg.\n");
+  }
+}
+
+void generateStringPrint(llvm::LLVMContext &context,
+                         llvm::Module &module,
+                         llvm::IRBuilder<> &builder,
+                         std::string toPrint,
+                         bool useGlobal ) {
+  printf("Error is here %d\n", i++);
+  llvm::Function *printFunct = module.getFunction("printStr");
+  printf("Error is here %d\n", i++);
+  llvm::Value *stringVar;
+  llvm::Constant *stringConstant =
+  llvm::ConstantDataArray::getString(context, toPrint);
+  printf("Error is here %d\n", i++);
+
+  //Ici il y a un probleme avec l'allocation de l'espace pour la variable à imprimer
+  //A mon avis d'ou le fait qu'il faille instancier des variables globales
+  //Du coup on aura à les load et pas les allouer
+    stringVar = builder.CreateAlloca(stringConstant->getType());
+  printf("Error is here %d\n", i++);
+    builder.CreateStore(stringConstant, stringVar);
+  printf("Error is here %d\n", i++);
+  
+
+  llvm::Value *cast = builder.CreatePointerCast(stringVar,
+                                                builder.getInt8PtrTy());
+  printf("Error is here %d\n", i++);
+  builder.CreateCall(printFunct, cast);
+  printf("Error is here %d\n", i++);
+}
 
 // The Pass itself
 namespace {
-  struct ProjetPass : public FunctionPass {
+  struct ProjetPass : public ModulePass {
     static char ID;
-    ProjetPass() : FunctionPass(ID) {}
+    ProjetPass() : ModulePass(ID) {}
 
-    virtual bool runOnFunction(Function &F) {
-      outs() << "I saw a function called " << F.getName() << "!\n";
-			for (auto& U : F.args())
-			{
-				outs() << "args of f are " << U << " \n";
-			}
-      return false;
-    }
+
+
+    virtual bool runOnModule(Module &M) 
+    { 
+      //Ici il faut ajouter les strings load et store aux variables globales
+
+      for (auto& Global : M.getGlobalList()){
+          
+       }
+
+      //Un example de fonction pour print un int et un string se trouve dans 
+       // llvm-5.0.0.src/examples/ExceptionDemo
+      llvm::LLVMContext Context;
+      llvm::IRBuilder<> builder(Context);
+      std::ostringstream bufferToPrint;
+      bufferToPrint << "Gen: Executing finally block ";
+
+      // generateStringPrint(Context, M, builder, bufferToPrint.str(), false);
+
+        for (Function &F : M)
+        {
+          for (BasicBlock &B : F) 
+          {
+            for (Instruction &I : B) 
+            {
+              if (isa<LoadInst>(I)) // || isa<StoreInst>(I))
+              {
+                IRBuilder<> builder(&I);
+                Value* ptr = I.getOperand(0);
+                // ArrayRef<Value*> args = {load, ptr};
+                // builder.createCall(printfFunction, args);
+
+                printf("Load instruction this is ptr\n");
+                ptr->print(errs());
+                printf(" \n");
+
+                printf("Load instruction this is I\n");
+                I.print(errs());
+                printf(" \n");
+              }
+              else 
+                if (isa<StoreInst>(I))
+              {
+                IRBuilder<> builder(&I);
+                //This gets the value to be stored
+                Value* val = I.getOperand(0);
+                //This gets the name of the variable, we need its address
+                Value* ptr = I.getOperand(1);
+                // ArrayRef<Value*> args = {store, ptr, val};
+                // builder.createCall(printfFunction, args);
+                printf("Store instruction this is val\n");
+                val->print(errs());
+                printf(" \n");
+                printf("Store instruction this is ptr\n");
+                ptr->print(errs());
+                printf(" \n");
+                printf("Store instruction this is I\n");
+                I.print(errs());
+                printf(" \n");
+              }
+
+            }
+          }
+        }
+      }
+    
   };
 }
 
--- test.c
+++ test.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int x = 5;
+
 int main()
 {
 
-	int array[5];
+	// int array[10];
 
-	for (int i =0; i<5;i++)
-		array[i]=i;
-
+	// for (int i =0; i<5;i++)
+	// 	array[i]=i;
+	int i = 63;
+	printf("Test %d\n", i);
 	return 0;
 }
