921bd8bffea4aba4cd7aad62eaa61d2fd3951feb
Cyrille <cyrille@cyrille-GE60-2PE>
2018-03-25 14:03:06 +0200

    derniere v

--- Projet.cpp
+++ Projet.cpp
@@ -28,7 +28,7 @@ void generateStringPrint(llvm::LLVMContext &context,
   printf("Error is here %d\n", i++);
   llvm::Function *printFunct = module.getFunction("printStr");
   printf("Error is here %d\n", i++);
-  llvm::Value *stringVar;
+  llvm::GlobalVariable *stringVar;
   llvm::Constant *stringConstant =
   llvm::ConstantDataArray::getString(context, toPrint);
   printf("Error is here %d\n", i++);
@@ -36,16 +36,26 @@ void generateStringPrint(llvm::LLVMContext &context,
   //Ici il y a un probleme avec l'allocation de l'espace pour la variable à imprimer
   //A mon avis d'ou le fait qu'il faille instancier des variables globales
   //Du coup on aura à les load et pas les allouer
-    stringVar = builder.CreateAlloca(stringConstant->getType());
-  printf("Error is here %d\n", i++);
-    builder.CreateStore(stringConstant, stringVar);
+  stringVar =
+    new llvm::GlobalVariable(module,
+                             stringConstant->getType(),
+                             true,
+                             llvm::GlobalValue::PrivateLinkage,
+                             stringConstant,
+                             "");
+    stringVar->print(errs());
+    printf("\n");
+    module.getGlobalList().push_back(stringVar);
+    // stringVar = builder.CreateAlloca(stringConstant->getType());
+  // printf("Error is here %d\n", i++);
+  //   builder.CreateStore(stringConstant, stringVar);
   printf("Error is here %d\n", i++);
   
 
   llvm::Value *cast = builder.CreatePointerCast(stringVar,
                                                 builder.getInt8PtrTy());
   printf("Error is here %d\n", i++);
-  builder.CreateCall(printFunct, cast);
+  // builder.CreateCall(printFunct, cast);
   printf("Error is here %d\n", i++);
 }
 
@@ -61,9 +71,9 @@ namespace {
     { 
       //Ici il faut ajouter les strings load et store aux variables globales
 
-      for (auto& Global : M.getGlobalList()){
+      // for (auto& Global : M.getGlobalList()){
           
-       }
+      //  }
 
       //Un example de fonction pour print un int et un string se trouve dans 
        // llvm-5.0.0.src/examples/ExceptionDemo
@@ -72,53 +82,68 @@ namespace {
       std::ostringstream bufferToPrint;
       bufferToPrint << "Gen: Executing finally block ";
 
-      // generateStringPrint(Context, M, builder, bufferToPrint.str(), false);
+       // generateStringPrint(Context, M, builder, bufferToPrint.str(), false);
 
-        for (Function &F : M)
-        {
-          for (BasicBlock &B : F) 
-          {
-            for (Instruction &I : B) 
-            {
-              if (isa<LoadInst>(I)) // || isa<StoreInst>(I))
-              {
-                IRBuilder<> builder(&I);
-                Value* ptr = I.getOperand(0);
-                // ArrayRef<Value*> args = {load, ptr};
-                // builder.createCall(printfFunction, args);
+      llvm::Constant *stringConstant =
+      llvm::ConstantDataArray::getString(Context, bufferToPrint.str());
+      printf("Error is here %d\n", i++);
 
-                printf("Load instruction this is ptr\n");
-                ptr->print(errs());
-                printf(" \n");
+  //Ici il y a un probleme avec l'allocation de l'espace pour la variable à imprimer
+  //A mon avis d'ou le fait qu'il faille instancier des variables globales
+  //Du coup on aura à les load et pas les allouer
+  GlobalVariable* stringVar =
+    new llvm::GlobalVariable(M,
+                             stringConstant->getType(),
+                             true,
+                             llvm::GlobalValue::PrivateLinkage,
+                             stringConstant,
+                             "");
 
-                printf("Load instruction this is I\n");
-                I.print(errs());
-                printf(" \n");
-              }
-              else 
-                if (isa<StoreInst>(I))
-              {
-                IRBuilder<> builder(&I);
-                //This gets the value to be stored
-                Value* val = I.getOperand(0);
-                //This gets the name of the variable, we need its address
-                Value* ptr = I.getOperand(1);
-                // ArrayRef<Value*> args = {store, ptr, val};
-                // builder.createCall(printfFunction, args);
-                printf("Store instruction this is val\n");
-                val->print(errs());
-                printf(" \n");
-                printf("Store instruction this is ptr\n");
-                ptr->print(errs());
-                printf(" \n");
-                printf("Store instruction this is I\n");
-                I.print(errs());
-                printf(" \n");
-              }
+        // for (Function &F : M)
+        // {
+        //   for (BasicBlock &B : F) 
+        //   {
+        //     for (Instruction &I : B) 
+        //     {
+        //       if (isa<LoadInst>(I)) // || isa<StoreInst>(I))
+        //       {
+        //         IRBuilder<> builder(&I);
+        //         Value* ptr = I.getOperand(0);
+        //         // ArrayRef<Value*> args = {load, ptr};
+        //         // builder.createCall(printfFunction, args);
 
-            }
-          }
-        }
+        //         printf("Load instruction this is ptr\n");
+        //         ptr->print(errs());
+        //         printf(" \n");
+
+        //         printf("Load instruction this is I\n");
+        //         I.print(errs());
+        //         printf(" \n");
+        //       }
+        //       else 
+        //         if (isa<StoreInst>(I))
+        //       {
+        //         IRBuilder<> builder(&I);
+        //         //This gets the value to be stored
+        //         Value* val = I.getOperand(0);
+        //         //This gets the name of the variable, we need its address
+        //         Value* ptr = I.getOperand(1);
+        //         // ArrayRef<Value*> args = {store, ptr, val};
+        //         // builder.createCall(printfFunction, args);
+        //         printf("Store instruction this is val\n");
+        //         val->print(errs());
+        //         printf(" \n");
+        //         printf("Store instruction this is ptr\n");
+        //         ptr->print(errs());
+        //         printf(" \n");
+        //         printf("Store instruction this is I\n");
+        //         I.print(errs());
+        //         printf(" \n");
+        //       }
+
+        //     }
+        //   }
+        // }
       }
     
   };
